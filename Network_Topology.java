65903d4dd418383fd24bfd01d7f9ca4cad17e002
UditNayak <uditnayak027@gmail.com>
2024-07-10 17:39:16 +0530

    Semaphore

--- Network_Topology.java
+++ Network_Topology.java
@@ -1,18 +1,22 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class Network_Topology {
   public static void main(String[] args) {
+    Semaphore pSemaphore = new Semaphore(3);
+    Semaphore cSemaphore = new Semaphore(0);
+
     Store store = new Store(5, 0);
     ExecutorService es = Executors.newCachedThreadPool();
 
     for (int i = 0; i < 100; i++) {
-      Producer p = new Producer(store, i);
+      Producer p = new Producer(store, i, pSemaphore, cSemaphore);
       es.execute(p);
     }
 
     for (int i = 0; i < 100; i++) {
-      Consumer c = new Consumer(store, i);
+      Consumer c = new Consumer(store, i, pSemaphore, cSemaphore);
       es.execute(c);
     }
     es.shutdown();
@@ -22,25 +26,34 @@ public class Network_Topology {
 class Producer implements Runnable {
   int id;
   private Store store;
+  Semaphore pSemaphore;
+  Semaphore cSemaphore;
 
-  public Producer(Store s, int i) {
+  public Producer(Store s, int i, Semaphore pSemaphore, Semaphore cSemaphore) {
     this.store = s;
     this.id = i;
+    this.pSemaphore = pSemaphore;
+    this.cSemaphore = cSemaphore;
   }
 
   @Override
   public void run() {
     while (true) {
-      synchronized (store) {
-        // producer seemaphore--
-        if (store.getCurrentSize() < store.getMaxSize()) {
-          System.out.println("Addition is done by Producer with id: " + id);
-          store.setCurrentSize(store.getCurrentSize() + 1);
-          System.out.println(
-              "                                                   store after the poll: " + store.getCurrentSize());
-        }
-        // consumer seemaphore++
+
+      // producer seemaphore--
+      try {
+        pSemaphore.acquire();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
+      if (store.getCurrentSize() < store.getMaxSize()) {
+        System.out.println("Addition is done by Producer with id: " + id);
+        store.setCurrentSize(store.getCurrentSize() + 1);
+        System.out.println(
+            "                                                   store after the poll: " + store.getCurrentSize());
+      }
+      // consumer seemaphore++
+      cSemaphore.release();
 
     }
   }
@@ -49,25 +62,35 @@ class Producer implements Runnable {
 class Consumer implements Runnable {
   private Store store;
   int id;
+  Semaphore pSemaphore;
+  Semaphore cSemaphore;
 
-  public Consumer(Store s, int i) {
+  public Consumer(Store s, int i, Semaphore pSemaphore, Semaphore cSemaphore) {
     this.store = s;
     this.id = i;
+    this.pSemaphore = pSemaphore;
+    this.cSemaphore = cSemaphore;
   }
 
   @Override
   public void run() {
     while (true) {
-      synchronized (store) {
-        // Consumer seemaphore--
-        if (store.getCurrentSize() > 0) {
-          System.out.println("Substraction is done by Consumer with id: " + id);
-          store.setCurrentSize(store.getCurrentSize() - 1);
-          System.out.println(
-              "                                                   store after the poll: " + store.getCurrentSize());
-        }
-        // producer seemaphore++
+
+      // Consumer seemaphore--
+      try {
+        cSemaphore.acquire();
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
       }
+      if (store.getCurrentSize() > 0) {
+        System.out.println("Substraction is done by Consumer with id: " + id);
+        store.setCurrentSize(store.getCurrentSize() - 1);
+        System.out.println(
+            "                                                   store after the poll: " + store.getCurrentSize());
+      }
+      // producer seemaphore++
+      pSemaphore.release();
 
     }
   }
